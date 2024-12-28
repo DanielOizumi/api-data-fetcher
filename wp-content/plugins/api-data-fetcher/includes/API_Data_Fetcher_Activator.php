8605a7a4b4e52b4791662eb5b2e65c2aa35fb579
DanielOizumi <daniel.oizumi@gmailcom>
2024-12-27 21:17:48 -0300

    Add WC requirement validation

--- wp-content/plugins/api-data-fetcher/includes/API_Data_Fetcher_Activator.php
+++ wp-content/plugins/api-data-fetcher/includes/API_Data_Fetcher_Activator.php
@@ -6,6 +6,15 @@ class API_Data_Fetcher_Activator
 {
   public static function activate()
   {
+    // Check if WooCommerce is active and interrupt the activation process
+    if (!in_array('woocommerce/woocommerce.php', apply_filters('active_plugins', get_option('active_plugins')))) {
+      add_action('admin_notices', function () {
+        echo '<div class="notice notice-error"><p>' . __('API Data Fetcher requires WooCommerce to be installed and activated.', 'api-data-fetcher') . '</p></div>';
+      });
+
+      wp_die(__('This plugin requires WooCommerce to be installed and activated.', 'api-data-fetcher'));
+    }
+
     // Add custom tab
     $custom_tab = new API_Data_Fetcher_Settings();
     $custom_tab->add_custom_endpoint();
